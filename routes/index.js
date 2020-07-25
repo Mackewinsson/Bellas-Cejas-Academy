6824376856e0496b222074e6c83b0e066ad23173
Mackewinsson <Mackewinsson@gmail.com>
2020-07-24 20:20:31 -0400

    first route added

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,29 @@
 var express = require("express");
 var router = express.Router();
+const { data } = require("../data/resultsObject.json");
+
+/* Handler function to wrap each route. */
+function asyncHandler(cb) {
+  return async (req, res, next) => {
+    try {
+      await cb(req, res, next);
+    } catch (error) {
+      next(error);
+    }
+  };
+}
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
   res.render("index", { title: "Client500" });
+});
+
+/* GET clients TESTING*/
+router.get("/clients", function (req, res, next) {
+  res.render("clients", {
+    title: "Client500",
+    data,
+  });
 });
 
 module.exports = router;
